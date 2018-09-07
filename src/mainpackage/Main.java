4c4bbce3b8bc5e570da229aee1ac4cb3bd599aba
Luke Oaks <oaksluke@gmail.com>
2018-09-07 13:17:25 -0600

    Fix all the things
    Fix 'em

---
+++ src/mainpackage/Main.java
@@ -0,0 +1,16 @@
+package mainpackage;
+
+import java.util.ArrayList;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		primes.add(0, 82);
+		primes.add(8);
+		primes.add(9, 9);
+		wait(2);
+		System.out.println(primes.);
+	}
+}
