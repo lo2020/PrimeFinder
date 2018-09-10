6064d00497460acfa27d15b2b7dfc48a0d214275
Luke Oaks <oaksluke@gmail.com>
2018-09-10 13:39:03 -0600

    Made the program work

--- src/mainpackage/Main.java
+++ src/mainpackage/Main.java
@@ -7,10 +7,33 @@ public class Main
 	public static void main(String[] args)
 	{
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		primes.add(0, 82);
-		primes.add(8);
-		primes.add(9, 9);
-		wait(2);
-		System.out.println(primes.);
+		primes.add(2);
+		System.out.println("2");
+
+		int test = 3;
+		int length;
+		boolean isprime;
+
+		while (true)
+		{
+			length = primes.size();
+			isprime = true;
+
+			for (int i = 0; i < length; i++)
+			{
+				if (test % primes.get(i) == 0)
+				{
+					isprime = false;
+					break;
+				}
+			}
+
+			if (isprime)
+			{
+				primes.add(test);
+				System.out.println(test);
+			}
+			test += 2;
+		}
 	}
 }
